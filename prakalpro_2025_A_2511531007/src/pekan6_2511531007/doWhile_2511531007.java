02009e5036105685072817f9b20012cf6d23987c
khaulaathifa2 <khaulaathifa2@gmail.com>
2025-11-03 14:40:30 +0700

    PRAKTIKUM PEKAN 6 KELAS A 2511531007

---
+++ prakalpro_2025_A_2511531007/src/pekan6_2511531007/doWhile_2511531007.java
@@ -0,0 +1,15 @@
+package pekan6_2511531007;
+
+import java.util.Scanner;
+
+public class doWhile_2511531007 {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner (System.in);
+			String phrase;
+			do {
+				System.out.print("Input Password: ");
+				phrase = console.next();
+			} while(!phrase.equals("abcd"));
+		}
+}
