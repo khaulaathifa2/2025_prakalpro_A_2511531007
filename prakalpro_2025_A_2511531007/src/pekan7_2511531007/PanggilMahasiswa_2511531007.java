ace488a6732ee0c902bf8729d4852880858d1bd9
khaulaathifa2 <khaulaathifa2@gmail.com>
2025-11-10 14:44:34 +0700

    PRAKTIKUM PEKAN 7 KELAS A 2511531007

---
+++ prakalpro_2025_A_2511531007/src/pekan7_2511531007/PanggilMahasiswa_2511531007.java
@@ -0,0 +1,14 @@
+package pekan7_2511531007;
+
+public class PanggilMahasiswa_2511531007 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511531007 a = new Mahasiswa_2511531007();
+		a.setNIM(1531007);
+		a.setNama("Tifa");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+
+}
---
+++ prakalpro_2025_A_2511531007/src/pekan7_2511531007/String1_2511531007.java
@@ -0,0 +1,14 @@
+package pekan7_2511531007;
+
+public class String1_2511531007 {
+
+	public static void main(String[] args) {
+		String salam = "Assalamu'alaikum";
+		System.out.println("panjang salam adalah : " + salam.length());
+		System.out.println(salam.toUpperCase()); //Outputs "ASSALAMU'ALAIKUM"
+		System.out.println(salam.toLowerCase()); //Outputs "assalamu'alaikum"
+		System.out.println(salam.indexOf("salam")); //Outputs 2
+
+	}
+
+}
